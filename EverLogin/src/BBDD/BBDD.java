ebfb82372aafe5d6206dca19de9fe169ca1cf5b9
ItsDaveTheWave <49502555+ItsDaveTheWave@users.noreply.github.com>
2019-05-09 17:27:34 +0200

    Update BBDD.java

--- EverLogin/src/BBDD/BBDD.java
+++ EverLogin/src/BBDD/BBDD.java
@@ -1,5 +1,25 @@
 package BBDD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class BBDD {
 
+	public static ResultSet getResultSet(String query)
+	{
+		try{
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+			Statement statement = connection.createStatement(); 
+			ResultSet resultSet = statement.executeQuery(query);
+			connection.close();  
+			
+			return resultSet;
+		}catch(Exception e) { 
+			System.out.println(e);
+		}
+		
+		return null;
+	}
 }
