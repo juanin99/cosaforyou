e49b307867ec2a69bef06c6c9ed61ef128ff5541
ItsDaveTheWave <49502555+ItsDaveTheWave@users.noreply.github.com>
2019-05-09 17:52:15 +0200

    Update GestorAceptar.java

--- EverLogin/src/Gestores/GestorAceptar.java
+++ EverLogin/src/Gestores/GestorAceptar.java
@@ -1,5 +1,37 @@
 package Gestores;
 
-public class GestorAceptar {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import Modelo.Usuario;
+import Vista.Login;
+import Vista.Resultado;
+
+public class GestorAceptar implements ActionListener
+{
+	private String username;
+	private Login login;
+	
+	public GestorAceptar(Login login)
+	{
+		this.login = login;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{	
+		username = login.getUserTextField().getText();
+		
+		if(GestorUsuario.userExists(username))
+		{
+			Resultado result = new Resultado("Resultado");
+			result.setUser(username);
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(login, "El usuario " + username + " no existe", "Usuario no existe", JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 }
