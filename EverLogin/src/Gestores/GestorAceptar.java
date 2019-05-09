cf4825345f0cb082e41fae23acdd305d01f65265
ItsDaveTheWave <49502555+ItsDaveTheWave@users.noreply.github.com>
2019-05-09 17:28:08 +0200

    Update GestorAceptar.java

--- EverLogin/src/Gestores/GestorAceptar.java
+++ EverLogin/src/Gestores/GestorAceptar.java
@@ -1,5 +1,23 @@
 package Gestores;
 
-public class GestorAceptar {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import Modelo.Usuario;
+
+public class GestorAceptar implements ActionListener
+{
+	private String username;
+	private Usuario user;
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{	
+		username = login.getUserTextField().getText();
+		user = new GestorUsuario().getUserFromDDBB(username);
+		
+		Resultado result = new Resultado("Resultado");
+		result.setUser(username);
+	}
 }
