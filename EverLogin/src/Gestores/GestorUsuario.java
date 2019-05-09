a6033c80afe5b5d57d3d1ac6d16448d6a5896b25
ItsDaveTheWave <49502555+ItsDaveTheWave@users.noreply.github.com>
2019-05-09 17:54:25 +0200

    Update GestorUsuario.java

--- EverLogin/src/Gestores/GestorUsuario.java
+++ EverLogin/src/Gestores/GestorUsuario.java
@@ -7,16 +7,11 @@ import BBDD.BBDD;
 import Modelo.Usuario;
 
 public class GestorUsuario 
-{
-	private BBDD bbdd;
-	
-	public GestorUsuario()
+{	
+	public static Usuario getUserFromDDBB(String username)
 	{
-		bbdd = new BBDD();
-	}
-	
-	public Usuario getUserFromDDBB(String username)
-	{
+		BBDD bbdd = new BBDD();
+		
 		ResultSet resultSet = bbdd.getResultSet("select * from usuario where nick = " + username);
 		try {
 			return new Usuario(resultSet.getString(1), resultSet.getString(2));
@@ -24,5 +19,18 @@ public class GestorUsuario
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public static boolean userExists(String username)
+	{
+		BBDD bbdd = new BBDD();
+		
+		try {
+			ResultSet resultSet = bbdd.getResultSet("select * from usuario where nick = " + username);
+			if(resultSet.getString(1) == username) return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
